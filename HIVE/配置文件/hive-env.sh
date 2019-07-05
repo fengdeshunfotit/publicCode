afdb7a3dfad6f7d41f8f9533ecbdfb0751330de1
冯德顺 <fengdeshunforit@163.com>
2019-07-05 14:15:04 +0800

    1

---
+++ HIVE/配置文件/hive-env.sh
@@ -0,0 +1,3 @@
+export  HADOOP_HOME=/home/hadoop/app/hadoop-2.6.0-cdh5.7.0
+export  HIVE_CONF_DIR=/home/hadoop/app/apache-hive-2.3.5-bin/conf
+export  HIVE_AUX_JARS_PATH=/home/hadoop/app/apache-hive-2.3.5-bin/lib
