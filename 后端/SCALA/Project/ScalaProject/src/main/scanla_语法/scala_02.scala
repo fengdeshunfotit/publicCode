b8c6d0b6806a0eb96be1456e51bfff1ebb33ef93
冯德顺 <fengdeshunforit@163.com>
2019-07-16 15:05:29 +0800

    新增
    新增

---
+++ 后端/SCALA/Project/ScalaProject/src/main/scanla_语法/scala_02.scala
@@ -0,0 +1,9 @@
+package main.scanla_语法
+
+object scala_02 {
+  def main(args: Array[String]): Unit = {
+    var name :String = "scala 语法标准";
+    var name01,name02 : String = "多个对象定义使用";
+    println(name);
+  }
+}
